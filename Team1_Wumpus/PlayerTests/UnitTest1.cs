b08571f584c26564b221140ae6801ffde7b6f964
Steven Lu <1050838@apps.nsd.org>
2019-04-26 10:00:56 -0700

    Added Player class

---
+++ Team1_Wumpus/PlayerTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PlayerTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Team1_Wumpus/Team1_Wumpus/Player.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Team1_Wumpus
+{
+    public class Player
+    {
+        public int GoldCoins { get; private set; }
+        public int Arrows { get; private set; }
+        public int TurnsTaken { get; private set; }
+        public int Score { get; private set; }
+        public bool IsWumpusDead { get; set; }
+
+
+        public Player()
+        {
+            Arrows = 3;
+            GoldCoins = 10;
+            TurnsTaken = 0;
+            IsWumpusDead = false;
+
+        }
+        public bool ShootArrow()
+        {
+            if (Arrows == 0)
+            {
+                return false;
+            }
+            else
+            {
+                Arrows--;
+                return false;
+            }
+        }
+
+        public void PurchaseArrow()
+        {
+            bool didSpendCoin = SpendCoin(1);
+            if (didSpendCoin)
+            {
+                Arrows++;
+            }
+        }
+
+        public void TakeATurn()
+        {
+            TurnsTaken++;
+        }
+
+        public bool SpendCoin(int numberToSpend)
+        {
+            if (numberToSpend > GoldCoins)
+            {
+                return false;
+            }
+            else
+            {
+                GoldCoins = GoldCoins - numberToSpend;
+                return true;
+            }
+        }
+
+        public void CalculateScore()
+        {
+            int maxPoints = 100;
+            int WumpusScore = 0;
+            if (IsWumpusDead)
+            {
+                WumpusScore = 50;
+            }
+
+            Score = maxPoints - TurnsTaken + GoldCoins + (5 * Arrows) + WumpusScore;
+        }
+
+        public void SetPlayerValuesForTesting(bool isWumpusDead, int arrows, int goldCoins, int turnsTaken)
+        {
+            IsWumpusDead = isWumpusDead;
+            Arrows = arrows;
+            GoldCoins = goldCoins;
+            TurnsTaken = turnsTaken;
+        }
+    }
+}
