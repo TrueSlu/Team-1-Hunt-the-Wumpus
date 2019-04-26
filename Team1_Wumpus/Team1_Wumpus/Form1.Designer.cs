f31fa20d40bc211754931eca991e007385bf06bc
Steven Lu <sjialu02@gmail.com>
2019-04-26 10:42:41 -0700

    Added tset console

--- Team1_Wumpus/Team1_Wumpus/Form1.Designer.cs
+++ Team1_Wumpus/Team1_Wumpus/Form1.Designer.cs
@@ -37,7 +37,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(85, 82);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Init classes";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
