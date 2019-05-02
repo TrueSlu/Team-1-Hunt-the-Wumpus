df12160b9f2176b16fbe9d64039262bd03ee9c61
Steven Lu <sjialu02@gmail.com>
2019-05-02 10:35:05 -0700

    Added some comments to lay out what's needed

--- Team1_Wumpus/Team1_Wumpus/HighScoreManager.cs
+++ Team1_Wumpus/Team1_Wumpus/HighScoreManager.cs
@@ -8,5 +8,12 @@ namespace Team1_Wumpus
 {
     class HighScoreManager
     {
+        //have a method to read high scores from a file, return list of "Score" objects to game control to display to UI
+
+        //have a method to write to file a new Score Object
+
+        //have a method to retrieve highest score
+
+        //have a method to check if a score given is high score
     }
 }
--- Team1_Wumpus/Team1_Wumpus/Trivia.cs
+++ Team1_Wumpus/Team1_Wumpus/Trivia.cs
@@ -8,6 +8,6 @@ namespace Team1_Wumpus
 {
     public class Trivia
     {
-
+        //maybe pop up a form w/ trivia questions. once trivia is finished, return if the event succeeded or failed. game control will pass the # of questions asked and the # right for you
     }
 }
