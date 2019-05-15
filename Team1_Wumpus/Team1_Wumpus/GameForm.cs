45e55b32acd95c869445df5988fcc8a9eb774cf8
Steven Lu <sjialu02@gmail.com>
2019-05-15 10:44:44 -0700

    Game changes

---
+++ Team1_Wumpus/Team1_Wumpus/GameForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Team1_Wumpus
+{
+    public partial class GameForm : Form
+    {
+
+        public Game GameObject { get; set; }
+        public GameForm()
+        {
+            InitializeComponent();
+        }
+
+        private void GameForm_Load(object sender, EventArgs e)
+        {
+            while (true)
+            {
+                MessageBox.Show("Hi");
+            }
+        }
+    }
+}
