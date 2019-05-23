a7ed0fb8754ce568b0f0b6bfb84ac09948450fcb
Raj Khanka <1101453@apps.nsd.org>
2019-05-23 10:35:42 -0700

    Read and Write to File for High Score

--- Team1_Wumpus/Team1_Wumpus/HighScoreManager.cs
+++ Team1_Wumpus/Team1_Wumpus/HighScoreManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -29,6 +30,38 @@ namespace Team1_Wumpus
             }
 
             return score;
+        }
+
+        public void WriteToFile()
+        {
+            string[] scores = { "Highest score", "Second-highest score", "Third-highest score" };
+            
+            string FetchScores =
+            Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(FetchScores, "WriteLines.txt")))
+            {
+                foreach (string score in scores)
+                    outputFile.WriteLine(score);
+            }
+
+        }
+
+        public void ReadToFile()
+        {
+            try
+            {   
+                using (StreamReader sr = new StreamReader("TestFile.txt"))
+                {
+                    String score = sr.ReadToEnd();
+                    Console.WriteLine(score);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
