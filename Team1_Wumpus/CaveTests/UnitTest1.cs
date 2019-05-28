b78498d628029e8fb225379d0cd0ac5a3a0f5670
Johan Olsson <1039011@apps.nsd.org>
2019-05-28 10:04:58 -0700

    grrr

--- Team1_Wumpus/CaveTests/UnitTest1.cs
+++ Team1_Wumpus/CaveTests/UnitTest1.cs
@@ -1,4 +1,6 @@
 using System;
+using System.IO;
+using System.Diagnostics;
 using Team1_Wumpus;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -13,35 +15,60 @@ namespace CaveTests
             var cave = new Cave(1);
             Assert.AreEqual(1, cave.CaveNumber);
         }
-    }
 
-    [TestClass]
-    public class CaveMethodTests
-    {
         [TestMethod]
-        public void TestAllConnections()
+        public void Write_File_AllConnect_Test()
         {
             var caveSys = new CaveSystem();
 
-            caveSys.WriteFile("allConnected");
+            caveSys.WriteFile("test.txt");
         }
-    }
-}
 
-namespace CaveSystemTests
-{
-    [TestClass]
-    public class CaveConstructorTests
-    {
         [TestMethod]
-        public void Cave_Number_Starts_Correct()
+        public void TestRandom()
         {
+            var caveSys = new CaveSystem('r');
+
+            caveSys.WriteFile("randCave.txt");
+        }
+
+        [TestMethod]
+        public void ReadFileTest()
+        {
+            var caveSys = new CaveSystem("allConnected.txt");
+
+            Trace.Listeners.Add(new TextWriterTraceListener(Console.Out));
+            // or Trace.Listeners.Add(new ConsoleTraceListener());
+            Trace.WriteLine("Hello World");
+
+            //CollectionAssert.AreEqual(caveSys.GetConnectedArray(1), new int[] { 25, 26, 2, 7, 6, 30 });
+            //CollectionAssert.AreEqual(caveSys.GetConnectedArray(2), new int[] { 26, 3, 9, 8, 7, 1 });
+            StreamWriter debug = new StreamWriter("readInValues.txt");
+            for (int i = 1; i <= 30; i++)
+            {
+                int[] ThisArray = caveSys.GetAdjacentArray(i);
+                debug.WriteLine("Cave Num: " + i.ToString() + String.Join(",", ThisArray));
+
+
+                switch (i)
+                {
+                    case 1:
+                        CollectionAssert.AreEqual(ThisArray, new int[] { 25, 26, 2, 7, 6, 30 });
+                        break;
+                    case 2:
+                        CollectionAssert.AreEqual(ThisArray, new int[] { 26, 3, 9, 8, 7, 1 });
+                        break;
+                    case 3:
+                        CollectionAssert.AreEqual(ThisArray, new int[] { 27, 28, 4, 9, 2, 26 });
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            debug.Close();
         }
     }
-
-    [TestClass]
-    public class CaveMethodTests
-    {
-
-    }
 }
+
+
