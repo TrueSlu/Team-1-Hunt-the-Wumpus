69dc8336df9c147350c1bce392f22fb8b14bd55d
Ransom Maeda <random.maeda@gmail.com>
2019-05-30 10:38:50 -0700

    ga

--- Team1_Wumpus/Team1_Wumpus/Trivia.cs
+++ Team1_Wumpus/Team1_Wumpus/Trivia.cs
@@ -8,6 +8,23 @@ namespace Team1_Wumpus
 {
     public class Trivia
     {
-        
+        public string Question { get; set; }
+        public string Answer1 { get; set; }
+        public string Answer2 { get; set; }
+        public string Answer3 { get; set; }
+        public int CorrectAnswer { get; set; }
+        public int UUID { get; private set; }
+
+
+
+        public Trivia(string q, string a1, string a2, string a3, int ca, int id)
+        {
+            Question = q;
+            Answer1 = a1;
+            Answer2 = a2;
+            Answer3 = a3;
+            CorrectAnswer = ca;
+            UUID = id;
+        }
     }
 }
